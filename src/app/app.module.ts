4eaace1dcaadc307d8c1c5e3cdd2c953036d0565
Macbook <pauloems@yahoo.com.br>
2018-04-19 01:31:43 -0300

    chore: estruturando projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,24 +6,49 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { EmpresaPage } from './../pages/cadastro/empresas/empresa';
+import { ContextMenu } from '../components/context-menu/context-menu';
+
+import { androidConfig } from './../config/android.config';
+import { iosConfig } from './../config/ios.config';
+import { PaisesProvider } from './../providers/providers';
+import { HttpModule } from '@angular/http';
+//import { Geolocation } from '@ionic-native/geolocation';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    ContextMenu,
+    EmpresaPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    HttpModule,
+    IonicModule.forRoot(MyApp, {
+      platforms: {
+          //Configurações de plataformas
+          android: androidConfig,
+          ios: iosConfig
+      }
+  })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    ContextMenu,
+    EmpresaPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    //Geolocation,
+    PaisesProvider,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
